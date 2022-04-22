bd820f7d3cf9123a070e6aee5b3a9d95d71d26ec
imag00 <ismael.magro2000@gmail.com>
2022-04-22 19:37:44 +0200

    Commit

---
+++ EjemploGit/Shared/EjemploGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploGitApp.swift
+//  Shared
+//
+//  Created by Alumno Swift on 22/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct EjemploGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
